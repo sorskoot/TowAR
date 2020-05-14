85afbb1d8cf9f4cc72b1559562db9fcdaf8d3c3d
Timmy Kokke <mental_c@hotmail.com>
2020-05-14 22:30:54 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log("Hello ♥ Console");
---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js'
+    },
+    plugins: [
+        new CopyPlugin([{
+            from: './src/static',
+            to: ''
+        }, ]),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                exclude: /node_modules/,
+                use: [
+                    'raw-loader',
+                    'glslify-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader', options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: 'sass-loader', options: {
+                        sourceMap: true
+                    }
+                }]
+            }
+        ]
+    }
+}
